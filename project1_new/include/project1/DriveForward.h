9c2a9f285d6bec2a0fccab5a48604033bef05ff9
jamesdawson31 <143676138+jamesdawson31@users.noreply.github.com>
2023-10-03 18:36:30 +1100

    Adding DriveForward header file to new project

---
+++ project1_new/include/project1/DriveForward.h
@@ -0,0 +1,29 @@
+#ifndef DRIVEFORWARD_H
+#define DRIVEFORWARD_H
+
+#include <iostream>
+#include <chrono>           // for measuring times
+
+class CDriveForward {
+    public:
+        // Constructor to get kp, ki, kd
+        CDriveForward(double kp, double ki, double kd) 
+                : kp_(kp), ki_(ki), kd_(kd), prevError_(0.0), integral_(0.0) {
+                    prevTime_ = std::chrono::high_resolution_clock::now();
+                };
+    
+
+        // PID calculation
+        double PIDController(double reference, double measured);
+
+    private:
+        // Important parameters
+        double kp_;         // proportional gain
+        double ki_;         // integral gain
+        double kd_;         // differential gain
+        double prevError_;
+        double integral_;
+        std::chrono::time_point<std::chrono::high_resolution_clock> prevTime_;
+};
+
+#endif // DRIVEFORWARD_H
