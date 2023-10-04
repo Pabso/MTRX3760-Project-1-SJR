0c5fb8722d7169b1637d2e31a0dc7a66016314b9
mkhr7002 <102570462+mkhr7002@users.noreply.github.com>
2023-10-05 01:34:45 +1100

    Add files via upload

---
+++ project1_new/src/convexState.cpp
@@ -0,0 +1,41 @@
+#include "../include/project1/convexState.h"
+
+convexState::convexState(ros::NodeHandle& nh) : nh_(nh) {
+    
+    // scan data to detect obstacles
+    laser_sub_ = nh_.subscribe("/scan", 1, &convexState::stopTurn, this);
+
+    // send Twist commands
+    cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+}
+
+void convexState::turnLeft() {
+    if (!turning_) {
+        
+        // if not already, start turning left
+        geometry_msgs::Twist cmd_vel;
+        cmd_vel.angular.z = 1.0;  // angular velocity
+        cmd_vel.linear.x = 0.2;   // linear velocity 
+        cmd_vel_pub_.publish(cmd_vel);
+        turning_ = true;
+    }
+}
+
+void convexState::stopTurn(const sensor_msgs::LaserScan::ConstPtr& scan) {
+    
+    // check for wall within the threshold
+    for (int i = 0; i < scan->ranges.size(); ++i) {
+        if (scan->ranges[i] < threshold_) {
+            
+            // when detected, stop turning and face forward
+            geometry_msgs::Twist cmd_vel;
+            cmd_vel.angular.z = 0.0;    // angular velocity
+            cmd_vel.linear.x = 0.0;     // linear velocity
+            cmd_vel_pub_.publish(cmd_vel);
+
+            // exit the state 
+            turning_ = false;
+            return;
+        }
+    }
+}
