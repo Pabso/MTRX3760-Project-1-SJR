6fb8ff8a07c7d0aa90c507ad18c6fca0eec44313
rmea8895 <79438179+rmea8895@users.noreply.github.com>
2023-10-05 10:24:34 +1100

    added handler to driveFoward class

--- project1_new/include/project1/DriveForward.h
+++ project1_new/include/project1/DriveForward.h
@@ -4,28 +4,44 @@
 #include <iostream>
 #include <chrono>           // for measuring times
 
+//
+#define NOMINAL_LINEAR_VELOCITY 0.2
+#define SLOW_LINEAR_VELOCITY 0.1
+
+// Foward declaration
+class CWallFollower;
+
 class CDriveForward {
-    public:
-        // Constructor to get kp, ki, kd
-        CDriveForward(double kp, double ki, double kd) 
-                : kp_(kp), ki_(ki), kd_(kd), prevError_(0.0), integral_(0.0) {
-                    prevTime_ = std::chrono::high_resolution_clock::now();
-                };
+  public:
+    // Constructor to get kp, ki, kd
+    CDriveForward(double kp, double ki, double kd) 
+            : kp_(kp), ki_(ki), kd_(kd), prevError_(0.0), integral_(0.0) {
+                prevTime_ = std::chrono::high_resolution_clock::now();
+            };
+
+
+    /**
+        * PID calculation member function
+        *
+        * @param[in] reference
+        * @param[in] measured
+        * @returns the 
+    */ 
     
+    double PIDController(double reference, double measured);
 
-        // PID calculation
-        double PIDController(double reference, double measured);
+    void handler(CWallFollower *wf);
 
-        bool handler();
-    private:
-        /// ------ Important PID parameters
-        double kp_;         // proportional gain
-        double ki_;         // integral gain
-        double kd_;         // differential gain
-        double prevError_;
-        double integral_;
-        std::chrono::time_point<std::chrono::high_resolution_clock> prevTime_;
-        /// ------ Important PID parameters
+  private:
+    
+    /// ------ Important PID parameters
+    double kp_;         // proportional gain
+    double ki_;         // integral gain
+    double kd_;         // differential gain
+    double prevError_;
+    double integral_;
+    std::chrono::time_point<std::chrono::high_resolution_clock> prevTime_;
+    /// ------ Important PID parameters
 };
 
 #endif // DRIVEFORWARD_H
