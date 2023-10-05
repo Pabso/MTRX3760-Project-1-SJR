d344473add2682a75b8fd205a228165faa4e66ed
mkhr7002 <102570462+mkhr7002@users.noreply.github.com>
2023-10-05 11:54:08 +1100

    Update convexState

--- project1_new/src/convexState.cpp
+++ project1_new/src/convexState.cpp
@@ -3,13 +3,13 @@
 convexState::convexState() {
     
     // scan data to detect obstacles
-    laser_sub_ = nh_.subscribe("/scan", 1, &convexState::stopTurn, this);
+    laser_sub_ = nh_.subscribe("/scan", 1, &convexState::turnLeft, this);
 
     // send Twist commands
     cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
 }
 
-void convexState::turnLeft() {
+void convexState::turnLeft(const sensor_msgs::LaserScan::ConstPtr& scan) {
     if (!turning_) {
         
         // if not already, start turning left
@@ -18,31 +18,28 @@ void convexState::turnLeft() {
         cmd_vel.linear.x = 0.2;   // linear velocity 
         cmd_vel_pub_.publish(cmd_vel);
         turning_ = true;
-    }
-}
 
-void convexState::stopTurn(const sensor_msgs::LaserScan::ConstPtr& scan) {
-    // get number of points
-    int pts = scan->ranges.size();
+        // get number of points
+        int pts = scan->ranges.size();
 
-    // make sure pts > 0
-    if (pts > 0) {
-
-        // check left pt
-        int leftIndex = 0; 
+        // make sure pts > 0
+        if (pts > 0) {
+            
+            // initlialise left pt
+            int leftIndex = 0; 
     
-        // check for wall within the threshold at left point
-        if (scan->ranges[leftIndex] < threshold_) {
+            // check for wall within the threshold at left point
+            if (scan->ranges[leftIndex] < threshold_) {
                 
-            // when detected, stop turning and face forward
-            geometry_msgs::Twist cmd_vel;
-            cmd_vel.angular.z = 0.0;    // angular velocity
-            cmd_vel.linear.x = 0.0;     // linear velocity
-            cmd_vel_pub_.publish(cmd_vel);
+                // when detected, stop turning
+                cmd_vel.angular.z = 0.0;    // angular velocity
+                cmd_vel.linear.x = 0.0;     // linear velocity
+                cmd_vel_pub_.publish(cmd_vel);
 
-            // exit the state 
-            turning_ = false;
-            return;
+                // exit the state 
+                turning_ = false;
+                return;
+            }
         }
     }
 }
