234f756009f3e302e78348818c97b1e3cc781cc3
mkhr7002 <102570462+mkhr7002@users.noreply.github.com>
2023-10-05 11:55:07 +1100

    Update convexState

--- project1_new/include/project1/convexState.h
+++ project1_new/include/project1/convexState.h
@@ -3,15 +3,14 @@
 
 #include "w./include/project1/wallFollower.h"
 
-//#include </opt/ros/noetic/share/ros/ros.h>                already in wallFOllower??
-//#include </opt/ros/noetic/share/geometry_msgs/Twist.h>    already in wallFOllower??
-//#include </opt/ros/noetic/share/sensor_msgs/LaserScan.h>  already in wallFOllower??
+//#include </opt/ros/noetic/share/ros/ros.h>                already in wallFollower??
+//#include </opt/ros/noetic/share/geometry_msgs/Twist.h>    already in wallFollower??
+//#include </opt/ros/noetic/share/sensor_msgs/LaserScan.h>  already in wallFollower??
 
 class convexState {
 public:
     convexState(ros::NodeHandle& nh);
-    void turnLeft();
-    void stopTurn(const sensor_msgs::LaserScan::ConstPtr& scan);
+    void turnLeft(const sensor_msgs::LaserScan::ConstPtr& scan);
 
 private:
     ros::NodeHandle nh_;
