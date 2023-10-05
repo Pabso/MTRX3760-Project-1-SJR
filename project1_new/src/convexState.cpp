307d4364509eaacd667739d7c269dfa22b72eb88
mkhr7002 <102570462+mkhr7002@users.noreply.github.com>
2023-10-05 20:56:05 +1100

    Update convexState.cpp

--- project1_new/src/convexState.cpp
+++ project1_new/src/convexState.cpp
@@ -1,36 +1,14 @@
 #include "../include/project1/convexState.h"
-
-
-void laserScan(const sensor_msgs::LaserScan::ConstPtr& msg) {   
-    
-    //get distance at angle 0 - left 
-    dist = msg->ranges.at(0)
-}
-
-void convexState::init() {
-    ros::Subscriber sub_laser = n.subscribe("/scan",1,laserScan);
-}
+#include<cmath>
 
 void convexState::turnLeft(CWallFollower* bot) {
 
-    // initalise lidar
-    init();
+    // calculate the angular velocity to achieve the 150mm radius arc
+    double linearVelocity = bot->linearV; 
+    double angularVelocity = linearVelocity / radius_;
 
-    if (!turning_) {
-        
-        // if not already, start turning left
-        updatecommandVelocity(1.0, 1.0); // linear, angular vel
-        turning_ = true;
-             
-        // check for wall within the threshold at left point
-        if (dist < threshold_) {
-                
-            // when detected, stop turning
-            updatecommandVelocity(0, 0);
+    bot->linearV = linearVelocity;
+    bot->angularV = angularVelocity;
 
-            // exit the state 
-            turning_ = false;
-            bot->States nextState = DRIVE_FOWARD;
-        }
-    }
+    return;
 }
