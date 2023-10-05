6c3d904393e5e2d5f2ec318a8252d8b94e6cdac8
pabso <yogpast45@gmail.com>
2023-10-05 16:44:26 +1100

    FInalised Concave module

--- project1_new/include/project1/ConcaveCorner.h
+++ project1_new/include/project1/ConcaveCorner.h
@@ -4,22 +4,16 @@
 #include "wallFollower.h"
 #include <cmath>
 
-#define DATA_READINGS 2
-
 class CConcaveCorner
 {
     public:
         void handler(CWallFollower* bot);
     private:
         void init(CWallFollower* bot);
-        void clbk_laser(const sensor_msgs::LaserScan::ConstPtr& msg);
+        int delta_angle_comp(CWallFollower* bot);
+        void laser_data_sraper(CWallFollower* bot);
 
-        int MIN_BOUNDRY_ANGLE = 10;
-        int MAX_BOUNDRY_ANGLE = 340;
-        int MAX_BOUNDRY_DIA;  //Cirucitlar Boundry of 150mm
-        int TURTLEBOT_LIDAR_OFFSET_X = 0;
-        int TURTLEBOT_LIDAR_OFFSET_Y = 0.01;
-        double lidar_Call[DATA_READINGS];
+        int laser_angle;
 
 };
 
--- project1_new/src/concave.cpp
+++ project1_new/src/concave.cpp
@@ -1,32 +1,40 @@
 #include "../include/project1/ConcaveCorner.h"
 
-void CConcaveCorner::clbk_laser(const sensor_msgs::LaserScan::ConstPtr& msg)
-{   
-    int increment = 0;
-    double dist;
-    double real_dist;
-    for (int num = MIN_BOUNDRY_ANGLE; num < MAX_BOUNDRY_ANGLE; num++)
-    {
-        //Account for offset of lidar
-        dist = msg->ranges.at(num)
-        real_dist = sqrt(pow((dist*sin(num)+TURTLEBOT_LIDAR_OFFSET_Y),2) + pow((dist*cos(num)+TURTLEBOT_LIDAR_OFFSET_X),2))
+// void CConcaveCorner::clbk_laser(const sensor_msgs::LaserScan::ConstPtr& msg)
+// {   
+//     // int increment = 0;
+//     // double dist;
+//     // double real_dist;
+//     // for (int num = MIN_BOUNDRY_ANGLE; num < MAX_BOUNDRY_ANGLE; num++)
+//     // {
+//     //     //Account for offset of lidar
+//     //     dist = msg->ranges.at(num);
+//     //     real_dist = sqrt(pow((dist*sin(num)+TURTLEBOT_LIDAR_OFFSET_Y),2) + pow((dist*cos(num)+TURTLEBOT_LIDAR_OFFSET_X),2));
 
-        ///If the scanned distance is inf set to range_max
-    if ((real_dist < MAX_BOUNDRY_DIA) && incrment < 2)
-        {
-        lidar_Call[increment] = num;
-        increment++;
-        }
-    }
-}
+//     //     ///If the scanned distance is inf set to range_max
+//     // if ((real_dist < MAX_BOUNDRY_DIA) && increment < 2)
+//     //     {
+//     //     lidar_Call[increment] = num;
+//     //     increment++;
+//     //     }
+//     // }
+// }
 
 void CConcaveCorner::init(CWallFollower* bot)
 {
-    //Set Boundry Size
-    MAX_BOUNDRY_DIA = bot->bubble_size_;
+    //Gather Lidar Data
+    laser_data_sraper(bot);
+}
 
-    //Initalise the Laser to gather information 
-    ros::Subscriber sub_laser = n.subscribe("scan",10, &CConcaveCorner::clbk_laser, this);
+void CConcaveCorner::laser_data_sraper(CWallFollower* bot)
+{   
+    for (const auto &pair : bot->mScanDataRange)
+    {
+        if(pair.second < 2*bot->bubble_size_ && pair.first > 20)
+        { 
+            laser_angle = pair.first;
+        }
+    }
 }
 
 void CConcaveCorner::handler(CWallFollower* bot)
@@ -34,14 +42,11 @@ void CConcaveCorner::handler(CWallFollower* bot)
     //initalise the Lidar
     init(bot);
 
-    //Average Angle
-    double delta_angle = (lidar_Call[0] + lidar_Call[1])/2;
-
     //Activate Turn
-    bot->turnOdom(delta_angle);
+    bot->turnOdom(laser_angle);
 
     //Set new State
-    bot->States nextState = DRIVE_FOWARD;
+    bot->nextState = bot->States::DRIVE_FOWARD;
 
     return;
 } 
